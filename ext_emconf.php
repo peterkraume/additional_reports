742038f16fdf437561cf89e24a59b33fc2d0f08d
Peter Kraume <peter.kraume@gmx.de>
2014-10-23 07:54:17 +0200

    improves dependencies for TYPO3 4.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,8 +32,9 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'php' => '5.0.0-5.5.99',
+			'php' => '5.3.0-5.5.99',
 			'typo3' => '4.5.0-6.2.99',
+			'fluid' => '1.3.1-0.0.0',
 		),
 		'conflicts' => array(
 		),
